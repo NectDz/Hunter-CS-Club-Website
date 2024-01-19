dd978e51bf58f51ba9c15084ba53350733787d4d
NectDz <54990613+NectDz@users.noreply.github.com>
2024-01-19 15:48:17 -0500

    CustomComponent

---
+++ src/Components/common/CommonButton/CommonButton.tsx
@@ -0,0 +1,34 @@
+import React, { ReactNode } from "react";
+import Button from "@mui/material/Button";
+
+interface CommonButtonProps {
+  children: ReactNode;
+  color?: string; // Accepts any string value
+  disabled?: boolean;
+  size?: string;
+  variant?: string;
+  sx?: string;
+}
+
+function CommonButton({
+  children,
+  color,
+  disabled,
+  size,
+  variant,
+  sx,
+}: CommonButtonProps) {
+  return (
+    <Button
+      color={color as any}
+      disabled={disabled}
+      size={size as any}
+      variant={variant as any}
+      sx={sx as any}
+    >
+      {children}
+    </Button>
+  );
+}
+
+export default CommonButton;
--- src/pages/Authentication/Authentication.tsx
+++ src/pages/Authentication/Authentication.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
-
+import CommonButton from "../../Components/common/CommonButton/CommonButton";
 const Authentication = () => {
   return (
     <Grid item xs={8}>
       This is authentication page.
+      <CommonButton variant="contained" size="small">
+        Test
+      </CommonButton>
     </Grid>
   );
 };
