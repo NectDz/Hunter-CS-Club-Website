1f40e3c8d8e295d8cfd4a5ce8d298571af72340e
NectDz <54990613+NectDz@users.noreply.github.com>
2024-02-03 09:45:43 -0500

    Added Content

--- src/pages/Gallery/Gallery.tsx
+++ src/pages/Gallery/Gallery.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Grid from "@mui/material/Grid";
+import { Typography } from "@mui/material";
 
 const Gallery = () => {
-  return <div>This is functions page.</div>;
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  return (
+    <Grid
+      container
+      spacing={8}
+      justifyContent="center"
+      alignItems="center"
+      style={{ marginTop: isMobile ? "60px" : "0px" }}
+    >
+      <Grid item xs={12}>
+        <Typography variant="h3" align="center" gutterBottom>
+          Gallery Page
+        </Typography>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Gallery;
--- src/pages/Resources/Resources.tsx
+++ src/pages/Resources/Resources.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Grid from "@mui/material/Grid";
+import { Typography } from "@mui/material";
 
 const Resources = () => {
-  return <div>This is hosting page.</div>;
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  return (
+    <Grid
+      container
+      spacing={8}
+      justifyContent="center"
+      alignItems="center"
+      style={{ marginTop: isMobile ? "60px" : "0px" }}
+    >
+      <Grid item xs={12}>
+        <Typography variant="h3" align="center" gutterBottom>
+          Resources Page
+        </Typography>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Resources;
