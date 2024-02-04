b412310676884580672a8edf214a1e104e4d82ee
NectDz <54990613+NectDz@users.noreply.github.com>
2024-02-03 23:50:31 -0500

    Removed Imports

--- src/Components/Navbar/consts/navbarItems.tsx
+++ src/Components/Navbar/consts/navbarItems.tsx
@@ -1,10 +1,3 @@
-import PeopleIcon from "@mui/icons-material/People";
-import ImageIcon from "@mui/icons-material/Image";
-import PublicIcon from "@mui/icons-material/Public";
-import SettingsEthernetIcon from "@mui/icons-material/SettingsEthernet";
-import SettingsInputComponentIcon from "@mui/icons-material/SettingsInputComponent";
-import DnsIcon from "@mui/icons-material/Dns";
-
 export const mainNavbarItems = [
   {
     id: 0,
