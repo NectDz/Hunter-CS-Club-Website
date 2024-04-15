3c824103a89228bf2e37c5be6f266c5f602f0ee0
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-04-15 15:25:35 -0400

    mod: created a new FAQ component to reuse it in home section

--- src/pages/FAQ/FAQ.tsx
+++ src/pages/FAQ/FAQ.tsx
@@ -1,11 +1,9 @@
 import { useTheme } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Grid from "@mui/material/Grid";
-import Accordion from "@mui/material/Accordion";
-import AccordionSummary from "@mui/material/AccordionSummary";
-import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
-import ControlPointOutlinedIcon from "@mui/icons-material/ControlPointOutlined";
+import FAQAccordion from "./Components/FAQAccordion";
+
 const FAQ = () => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
@@ -13,48 +11,57 @@ const FAQ = () => {
   //questions and answers list
   const faqs = [
     {
+      id: "f1",
       question: "What does the Computer Science Club offer?",
       answer:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco ",
     },
     {
+      id: "f2",
       question: "How can i get involved with the Computer Science Club?",
       answer:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco ",
     },
     {
+      id: "f3",
       question:
         "Is this club for beginners? Can i still be a member if I am a non-Computer Science major?",
       answer:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco ",
     },
     {
+      id: "f4",
       question: "Will this club help with classwork?",
       answer:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco ",
     },
     {
+      id: "f5",
       question: "What upcoming Spring semester events should i know about?",
       answer:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco ",
     },
     {
+      id: "f6",
       question:
         "Where is the Computer Science Club meeting room and what are their hours?",
       answer:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco ",
     },
     {
+      id: "f7",
       question: "Will there be internship opportunities?",
       answer:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco ",
     },
     {
+      id: "f8",
       question: "Will this club help with interview skills?",
       answer:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco ",
     },
     {
+      id: "f9",
       question: "Will this club have guest speakers?",
       answer:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
@@ -91,12 +98,12 @@ const FAQ = () => {
           </Typography>
           {/* Questions  */}
           <div>
-            {faqs.map((data, index) => {
+            {faqs.map((faq, index) => {
               return (
-                <Accordion
+                <FAQAccordion
+                  faq={faq}
+                  key={faq.id}
                   style={{
-                    border: "1px solid #9859AC",
-                    padding: "15px 15px 15px 0px",
                     borderTopRightRadius: index === 0 ? "10px" : "0",
                     borderTopLeftRadius: index === 0 ? "10px" : "0",
                     borderBottomLeftRadius:
@@ -104,23 +111,7 @@ const FAQ = () => {
                     borderBottomRightRadius:
                       index === faqs.length - 1 ? "10px" : "0",
                   }}
-                  key={index}
-                >
-                  <AccordionSummary
-                    expandIcon={
-                      <ControlPointOutlinedIcon htmlColor="#9859AC" />
-                    }
-                    aria-controls={`panel${index + 1}-content`}
-                    id={`panel${index + 1}-header`}
-                  >
-                    {/* faq question */}
-                    <Typography fontWeight={600}>{data?.question}</Typography>
-                  </AccordionSummary>
-                  <AccordionDetails>
-                    {/* faq answer  */}
-                    <Typography>{data?.answer}</Typography>
-                  </AccordionDetails>
-                </Accordion>
+                />
               );
             })}
           </div>
