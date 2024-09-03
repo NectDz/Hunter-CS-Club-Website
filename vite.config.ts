64776ab1357c0658eb79b19deb491a47091ac875
Justin5802 <93137213+Justin5802@users.noreply.github.com>
2024-09-03 08:33:10 -0400

    Reverted changes to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-svg-loader";
 
-export default {
-  server: {
-    hmr: {
-      overlay: false
-    }
-  }
-}
+export default defineConfig({
+  plugins: [react(), svgr()],
+});
