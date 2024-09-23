f7130ba788eb455d178b2b0a210499686570f470
Justin5802 <93137213+Justin5802@users.noreply.github.com>
2024-09-23 17:53:39 -0400

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import svgr from "vite-svg-loader";
-
-export default defineConfig({
-  plugins: [react(), svgr()],
-});
+export default {
+  server: {
+    hmr: {
+      overlay: false
+    }
+  }
+}
